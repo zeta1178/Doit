bc648315edcb0682bf0ed792d29a83de3aeb7d06
Michael Cruz <michaelcruz@aim.com>
2016-09-14 15:32:32 -0400

    sec 6 let 62

---
+++ Doit/CreatTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreatTaskViewController.swift
+//  Doit
+//
+//  Created by Michael Cruz on 9/14/16.
+//  Copyright © 2016 MIchael Cruz. All rights reserved.
+//
+
+import UIKit
+
+class CreatTaskViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(sender: AnyObject) {
+    }
+   
+
+}
